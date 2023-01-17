966a2c26a6e40b409e7758fc8f6e30640da0e3db
Ethan Yin <yinssh1005@gmail.com>
2023-01-17 12:40:48 +0800

    init

---
+++ new-module/src/main/java/com/sample/NewModuleUtil.java
@@ -0,0 +1,23 @@
+package com.sample;
+
+public class NewModuleUtil {
+
+    String utilName;
+    String utilID;
+
+    public String getUtilName() {
+        return utilName;
+    }
+
+    public void setUtilName(String utilName) {
+        this.utilName = utilName;
+    }
+
+    public String getUtilID() {
+        return utilID;
+    }
+
+    public void setUtilID(String utilID) {
+        this.utilID = utilID;
+    }
+}
